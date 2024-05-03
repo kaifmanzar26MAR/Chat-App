064654c4e0ee165bfefd359407efdbc9b5e0e9c1
Sourabh <sourabh4pvt@gmail.com>
2024-05-03 22:10:22 +0530

    made the app ready to get deployed

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming request with JSON payload (from req.body)
@@ -18,6 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 // app.get("/", (req, res) => {
 //   res.send("Server is running");
