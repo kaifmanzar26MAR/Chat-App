301b7942902d5d7f538c92eeb1b42e62d3ed66a8
Sourabh <sourabh4pvt@gmail.com>
2024-05-04 14:06:33 +0530

    added /api

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 })
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Server is running");
 });
 
